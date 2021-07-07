cdaaa2f9d8d2e2ba668f51ba4711b316cea20488
Berkay <berkay.ergun16@gmail.com>
2021-07-07 10:13:19 +0300

    03 end

--- my-app/src/BookList.js
+++ my-app/src/BookList.js
@@ -1,30 +1,37 @@
-import React from 'react'
-import Book from './Book'
-import './BookList.css';
+import React from "react";
+import Book from "./Book";
+import "./BookList.css";
+import BookContext from "./contexts/BookContext";
 
 export default class BookList extends React.Component {
+  static contextType = BookContext;
 
-    render() {
+  render() {
+    const books = this.context;
 
-        const bookList = this.props.books.map((book,i)=>{
-            return <Book book={book} key={i} />
-        })
-        return (
-              <section className="page-section"  id="portfolio">
-                        <div className="container">
-                            <div className="text-center">
-                                <h2 className="section-heading text-uppercase">BookFolio</h2>
-                                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-                                <button type="button" className="btn btn-sm btn-info" style={{marginTop: '-70px'}}
-                                >Change Theme</button>
-                            </div>
-                            <div className="row">
-        
-                            {bookList}
-        
-                            </div>
-                        </div>
-                    </section>
-        )
-    }
+    return (
+      <section className="page-section" id="portfolio">
+        <div className="container">
+          <div className="text-center">
+            <h2 className="section-heading text-uppercase">BookFolio</h2>
+            <h3 className="section-subheading text-muted">
+              Lorem ipsum dolor sit amet consectetur.
+            </h3>
+            <button
+              type="button"
+              className="btn btn-sm btn-info"
+              style={{ marginTop: "-70px" }}
+            >
+              Change Theme
+            </button>
+          </div>
+          <div className="row">
+            {books.map((book, i) => {
+              return <Book book={book} key={i} />;
+            })}
+          </div>
+        </div>
+      </section>
+    );
+  }
 }
